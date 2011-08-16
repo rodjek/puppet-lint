74ac702744bbb1cab5f8bce1ffd8916a248ee367
Tim Sharpe <tim@sharpe.id.au>
2011-08-15 20:33:46 -0700

    String#each_line strips trailing whitespace

--- lib/puppet-lint/plugins/check_whitespace.rb
+++ lib/puppet-lint/plugins/check_whitespace.rb
@@ -1,7 +1,7 @@
 class PuppetLint::Plugins::CheckWhitespace < PuppetLint::CheckPlugin
   def test(data)
     line_no = 0
-    data.each_line do |line|
+    data.split("\n").each do |line|
       line_no += 1
       warn "tab character found on line #{line_no}" if line.include? "\t"
       warn "trailing whitespace found on line #{line_no}" if line.end_with? " "
