870614b88bfd38c7b560eb7f323e444db9aecb6b
Tim Sharpe <tim@sharpe.id.au>
2011-08-15 20:36:38 -0700

    Warn if lines > 80c

--- lib/puppet-lint/plugins/check_whitespace.rb
+++ lib/puppet-lint/plugins/check_whitespace.rb
@@ -5,6 +5,7 @@ class PuppetLint::Plugins::CheckWhitespace < PuppetLint::CheckPlugin
       line_no += 1
       warn "tab character found on line #{line_no}" if line.include? "\t"
       warn "trailing whitespace found on line #{line_no}" if line.end_with? " "
+      warn "line #{line_no} has more than 80 characters" if line.length > 80
     end
   end
 end
