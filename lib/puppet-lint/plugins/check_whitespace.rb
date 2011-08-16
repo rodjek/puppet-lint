10eaa5d4d5bcc0328f3dab31dcac2e4222986623
Tim Sharpe <tim@sharpe.id.au>
2011-08-15 20:47:43 -0700

    Check that two-space soft tabs are being used

--- lib/puppet-lint/plugins/check_whitespace.rb
+++ lib/puppet-lint/plugins/check_whitespace.rb
@@ -3,9 +3,15 @@ class PuppetLint::Plugins::CheckWhitespace < PuppetLint::CheckPlugin
     line_no = 0
     data.split("\n").each do |line|
       line_no += 1
-      warn "tab character found on line #{line_no}" if line.include? "\t"
-      warn "trailing whitespace found on line #{line_no}" if line.end_with? " "
+      error "tab character found on line #{line_no}" if line.include? "\t"
+      error "trailing whitespace found on line #{line_no}" if line.end_with? " "
       warn "line #{line_no} has more than 80 characters" if line.length > 80
+
+      line.scan(/^ +/) do |prefix|
+        unless prefix.length % 2 == 0
+          error "two-space soft tabs not used on line #{line_no}"
+        end
+      end
     end
   end
 end
