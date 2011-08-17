d20cdb88d7a848d206eb716146345f80b8a162ad
Tim Sharpe <tim@sharpe.id.au>
2011-08-16 19:12:39 -0700

    Check that ensure is the first attribute in a resource if it's specified

--- lib/puppet-lint/plugins/check_resources.rb
+++ lib/puppet-lint/plugins/check_resources.rb
@@ -5,20 +5,32 @@ class PuppetLint::Plugins::CheckResources < PuppetLint::CheckPlugin
   def test(data)
     line_no = 0
     in_resource = true
+    first_attribute = false
     data.split("\n").each do |line|
       line_no += 1
 
       if line.include? "{"
         in_resource = true
+        first_attribute = true
         line = line.slice(line.index('{')..-1)
       end
 
       if in_resource
         # Resource titles SHOULD be quoted
         line.scan(/[^'"]\s*:/) do |match|
-          unless line =~ /\$\w+\s*:/
+          unless line =~ /\$[\w:]+\s*:/
             warn "unquoted resource title on line #{line_no}"
           end
+        end
+
+        line.scan(/(\w+)\s*=>\s*([^\n,;]+)/) do |attr, value|
+          # Ensure SHOULD be the first attribute listed
+          if attr == 'ensure'
+            unless first_attribute
+              warn "ensure found on line #{line_no} but it's not the first attribute"
+            end
+          end
+          first_attribute = false
         end
       end
 
