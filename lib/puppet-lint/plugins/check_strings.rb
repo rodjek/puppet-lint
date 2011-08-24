60e686919d30accb1beee352d216591e83010dec
Tim Sharpe <tim@sharpe.id.au>
2011-08-24 14:53:58 -0700

    tokenise all the things

--- lib/puppet-lint/plugins/check_strings.rb
+++ lib/puppet-lint/plugins/check_strings.rb
@@ -1,38 +1,54 @@
-class PuppetLint::Plugins::CheckStrings < PuppetLint::CheckPlugin
-  def test(data)
-    line_no = 0
-    data.each_line do |line|
-      line_no += 1
-      line.match(/"([^\\"]|\\\\|\\")*"/).to_a.each do |s|
-        if s.is_a? String and s.start_with? '"'
-          variable_found = false
-          s.scan(/.\$./) do |w|
-            if w.start_with? '\\'
-              next
-            elsif w.end_with? '{'
-              variable_found = true
-            else
-              warn "variable not enclosed in {} on line #{line_no}"
-            end
-          end
-          unless variable_found
-            warn "double quoted string containing no variables on line #{line_no}"
-          end
-          if s =~ /^"\$\{[\w\:]+\}"$/
-            warn "string containing only a variable on line #{line_no}"
-          end
+require 'puppet'
 
-          line = line[line.index('"', line.index('"')+1)..-1]
+class PuppetLint::Plugins::CheckStrings < PuppetLint::CheckPlugin
+  class ::Puppet::Parser::Lexer
+    class TokenList
+      def del_token(token)
+        @tokens.delete(token)
+      end
+    end
+
+    TOKENS.add_tokens("<single quotes string>" => :SSTRING)
+    TOKENS.del_token(:SQUOTE)
+
+    TOKENS.add_token :SQUOTE, "'" do |lexer, value|
+      [TOKENS[:SSTRING], lexer.slurpstring(value,["'"],:ignore_invalid_escapes).first ]
+    end
+  end
+
+  def test(data)
+    l = Puppet::Parser::Lexer.new
+    l.string = data
+    tokens = l.fullscan
+
+    tokens.each_index do |token_idx|
+      token = tokens[token_idx]
+
+      if token.first == :STRING
+        contents = token.last[:value]
+        line_no = token.last[:line]
+        variable_found = false
+
+        contents.scan(/.\$./) do |w|
+          if w.start_with? '\\'
+            next
+          elsif w.end_with? '{'
+            variable_found = true
+          else
+            warn "variable not enclosed in {} on line #{line_no}"
+          end
+        end
+        unless variable_found
+          warn "double quoted string containing no variables on line #{line_no}"
         end
       end
 
-      line.match(/'.+?'/).to_a.each do |s|
-        if s.start_with? "'"
-          s.scan(/\$./) do |w|
-            if w.end_with? '{'
-              error "single quoted string containing a variable found on line #{line_no}"
-            end
-          end
+      if token.first == :SSTRING
+        contents = token.last[:value]
+        line_no = token.last[:line]
+
+        if contents.include? '${'
+          error "single quoted string containing a variable found on line #{line_no}"
         end
       end
     end
