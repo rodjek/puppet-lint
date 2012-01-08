3c5e8528cfa81ba8f938ce7595ea0d13c302d810
Jan Vansteenkiste <jan@vstone.eu>
2012-01-08 11:47:45 +0100

    Introduced log format and some helper methods.
    bin/puppet-lint
    * Added the --log-format option.
      Added documentation to the --help output.
    
    lib/puppet-lint.rb
    * Check for 'string' % {hash} functionality and basic backport.
    
    * Added helper methods and the log-format configuration option to the main PuppetLint class.
      - log_format:     Gets the configured log-format.
                        Tries to mimic the old behavior if no
                        log-format has been set.
    
      - format_message: Format the message.

--- lib/puppet-lint.rb
+++ lib/puppet-lint.rb
@@ -18,6 +18,59 @@ unless String.respond_to?('prepend')
   end
 end
 
+# If we are using an older ruby version, we back-port the basic functionality
+# we need for formatting output: 'somestring' % <hash>
+begin
+  if ('%{test}' % {:test => 'replaced'} == 'replaced')
+    # If this works, we are all good to go.
+  end
+rescue
+  # If the test failed (threw a error), monkeypatch String.
+  # Most of this code came from http://www.ruby-forum.com/topic/144310 but was
+  # simplified for our use.
+
+  # Basic implementation of 'string' % { } like we need it. needs work.
+  class String
+    Percent = instance_method '%' unless defined? Percent
+    def % *a, &b
+      a.flatten!
+
+      string = case a.last
+      when Hash
+        expand a.pop
+      else
+        self
+      end
+
+      if a.empty?
+        string
+      else
+        Percent.bind(string).call(*a, &b)
+      end
+
+    end
+    def expand! vars = {}
+      loop do
+        changed = false
+        vars.each do |var, value|
+          var = var.to_s
+          var.gsub! %r/[^a-zA-Z0-9_]/, ''
+          [
+            %r/\%\{#{ var }\}/,
+          ].each do |pat|
+            changed = gsub! pat, "#{ value }"
+          end
+        end
+        break unless changed
+      end
+      self
+    end
+    def expand opts = {}
+      dup.expand! opts
+    end
+  end
+end
+
 class PuppetLint::NoCodeError < StandardError; end
 
 class PuppetLint
@@ -49,6 +102,23 @@ class PuppetLint
 
   def code=(value)
     @data = value
+  end
+
+  def log_format
+    if configuration.log_format == ''
+      ## recreate previous old log format as far as thats possible.
+      format = '%{KIND}: %{message} on line %{linenumber}'
+      if configuration.with_filename
+        format.prepend '%{path} - '
+      end
+      configuration.log_format = format
+    end
+    return configuration.log_format
+  end
+
+  def format_message(message)
+    format = log_format
+    puts format % message
   end
 
   def report(kind, message)
@@ -104,5 +174,6 @@ end
 PuppetLint.configuration.fail_on_warnings = false
 PuppetLint.configuration.error_level = :all
 PuppetLint.configuration.with_filename = false
+PuppetLint.configuration.log_format = ''
 
 require 'puppet-lint/plugins'
