22224c1c0673a9109143f442f09796de501b8546
Jan Vansteenkiste <jan@vstone.eu>
2012-01-08 12:38:08 +0100

    Added RSpec Matcher for making plugin testing easier with problems
    * Implemented filter_array_of_hashes (helper) function.
      - See source for an example.
    * Implemented have_problem check.
      - Checks if a problem exists in the array that matches the filter hash.
      - Add readable failure messages.
    * Implemented only_have_problem check.
      - Checks if the problem array only exists out of elements matching the filter hash.
      - Add readable failure messages.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,3 +9,101 @@ require 'puppet-lint'
 #PuppetLint::CheckPlugin.any_instance.stub(:error) do |arg|
 #  raise PuppetLint::Error
 #end
+
+#     filter_array_of_hashes(array, filter) -> an_array
+#
+# Filters out hashes by applying filter_hash to each hash
+# in the array. All set value/key pairs in filter_hash must
+# match before a hash is allowed.
+# Returns all hashes that matched in an array.
+#
+#   filter_array_of_hashes(
+#     [
+#       {:filter => 1, :name => 'one'},
+#       {:filter => 2, :name => 'two'},
+#       {:filter => 3, :name => 'three'},
+#     ],
+#     { :filter => 2 }
+#   )
+#   => [{:filter=>2, :name=>"two"}]
+#
+#   filter_array_of_hashes([{:f => 1}, {:f => 2}], {})
+#   => [{:f=>1}, {:f=>2}]
+#
+def filter_array_of_hashes(array_of_hashes, filter_hash)
+  array_of_hashes.select { |hash_to_check|
+    val = true
+    filter_hash.each do |k,v|
+      if ! hash_to_check.key?(k)
+        val = false
+        break
+      elsif hash_to_check[k].to_s != v.to_s
+        val = false
+        break
+      end
+    end
+    val
+  }
+end
+
+RSpec::Matchers.define :have_problem do |filter|
+
+  match do |problems|
+    filter_array_of_hashes(problems, filter).length > 0
+  end
+
+  failure_message_for_should do |problems|
+    message = "could not find any problems matching the filter."
+    message << "
+    * filter = #{filter.inspect}
+    * problems = [
+    "
+    problems.each { |prob| message << "    #{prob.inspect}," }
+    message << "
+      ]"
+    message
+  end
+
+  failure_message_for_should_not do |problems|
+    message = "some problems matched the filter."
+    message << "
+    * filter = #{filter.inspect}
+    * matched = [
+    "
+    filter_array_of_hashes(problems, filter).each { |prob| message << "    #{prob.inspect}," }
+    message << "
+      ]"
+    message
+  end
+
+end
+
+RSpec::Matchers.define :only_have_problem do |filter|
+
+  match do |actual|
+    res = filter_array_of_hashes(actual, filter)
+    res.length == actual.length
+  end
+
+  failure_message_for_should do |problems|
+    left = problems - filter_problems(actual, filter)
+    message = "There were problems not matching filter."
+    message << "
+    * filter = #{filter.inspect}
+    * unmatched = [
+    "
+    left.each { |prob| message << "    #{prob.inspect}," }
+    message << "
+      ]"
+    message
+  end
+
+  failure_message_for_should_not do |problems|
+    message = "There were no problems found besides the ones matched with filter."
+    message << "
+    * filter = #{filter.inspect}
+    "
+    message
+  end
+
+end
