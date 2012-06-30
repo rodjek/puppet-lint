f0a1a077b38913b463cf3ddb5f3f632e0cf3cafe
Tim Sharpe <tim@sharpe.id.au>
2012-06-30 22:33:26 +1000

    Clean up CheckVariables tests

--- spec/puppet-lint/check_variables_spec.rb
+++ spec/puppet-lint/check_variables_spec.rb
@@ -3,19 +3,31 @@ require 'spec_helper'
 describe PuppetLint::Plugins::CheckVariables do
   subject do
     klass = described_class.new
-    klass.run(defined?(fullpath).nil? ? {:fullpath => ''} : {:fullpath => fullpath}, code)
+    fileinfo = {}
+    fileinfo[:fullpath] = defined?(fullpath).nil? ? '' : fullpath
+    klass.run(fileinfo, code)
     klass
   end
 
   describe 'a variable containing a dash' do
-    let(:code) { "$foo-bar" }
+    let(:code) { '$foo-bar' }
 
-    its(:problems) { should have_problem :kind => :warning, :message => "variable contains a dash", :linenumber => 1 }
+    its(:problems) { should have_problem({
+      :kind       => :warning,
+      :message    => 'variable contains a dash',
+      :linenumber => 1,
+      :column     => 1,
+    }) }
   end
 
   describe 'variable containing a dash' do
     let(:code) { '" $foo-bar"' }
 
-    its(:problems) { should have_problem :kind => :warning, :message => "variable contains a dash", :linenumber => 1 }
+    its(:problems) { should have_problem({
+      :kind       => :warning,
+      :message    => 'variable contains a dash',
+      :linenumber => 1,
+      :column     => 3,
+    }) }
   end
 end
