f178f1013e22c69e45fe68568b06cfebcf2a0f3f
Tim Sharpe <tim@sharpe.id.au>
2012-06-30 22:19:09 +1000

    Rewrite variable_contains_dash check to use new lexer

--- lib/puppet-lint/plugins/check_variables.rb
+++ lib/puppet-lint/plugins/check_variables.rb
@@ -1,23 +1,18 @@
 class PuppetLint::Plugins::CheckVariables < PuppetLint::CheckPlugin
+  # Public: Test the manifest tokens for variables that contain a dash and
+  # record a warning for each instance found.
+  #
+  # Returns nothing.
   check 'variable_contains_dash' do
-    tokens.each_index do |token_idx|
-      token = tokens[token_idx]
-
-      if token.first == :VARIABLE
-        variable = token.last[:value]
-        line_no = token.last[:line]
-        if variable.match(/-/)
-          notify :warning, :message =>  "variable contains a dash", :linenumber => line_no
-        end
-      end
-
-      if token.first == :DQPRE
-        end_of_string_idx = tokens[token_idx..-1].index { |r| r.first == :DQPOST }
-        tokens[token_idx..end_of_string_idx].each do |t|
-          if t.first == :VARIABLE and t.last[:value].match(/-/)
-            notify :warning, :message =>  "variable contains a dash", :linenumber => t.last[:line]
-          end
-        end
+    tokens.select { |r|
+      [:VARIABLE, :UNENC_VARIABLE].include? r.type
+    }.each do |token|
+      if token.value.match(/-/)
+        notify :warning, {
+          :message    => 'variable contains a dash',
+          :linenumber => token.line,
+          :column     => token.column,
+        }
       end
     end
   end
