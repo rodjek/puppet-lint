2b39d30fc2af17bd2f0f8e1cc51af87083881eed
Tim Sharpe <tim@sharpe.id.au>
2012-07-07 20:38:46 +1000

    Update variable_scope check

--- lib/puppet-lint/plugins/check_classes.rb
+++ lib/puppet-lint/plugins/check_classes.rb
@@ -125,22 +125,38 @@ class PuppetLint::Plugins::CheckClasses < PuppetLint::CheckPlugin
   end
 
   check 'variable_scope' do
+    variables_in_scope = [
+      'name',
+      'title',
+      'module_name',
+      'environment',
+      'clientcert',
+      'clientversion',
+      'servername',
+      'serverip',
+      'serverversion',
+      'caller_module_name',
+    ]
     (class_indexes + defined_type_indexes).each do |idx|
       object_tokens = tokens[idx[:start]..idx[:end]]
-      variables_in_scope = ['name', 'title', 'module_name', 'environment', 'clientcert', 'clientversion', 'servername', 'serverip', 'serverversion', 'caller_module_name']
+      object_tokens.reject! { |r| r.type == :WHITESPACE}
       referenced_variables = []
-      header_end_idx = object_tokens.index { |r| r.first == :LBRACE }
-      lparen_idx = object_tokens[0..header_end_idx].index { |r| r.first == :LPAREN }
-      rparen_idx = object_tokens[0..header_end_idx].rindex { |r| r.first == :RPAREN }
+      header_end_idx = object_tokens.index { |r| r.type == :LBRACE }
+      lparen_idx = object_tokens[0..header_end_idx].index { |r|
+        r.type == :LPAREN
+      }
+      rparen_idx = object_tokens[0..header_end_idx].rindex { |r|
+        r.type == :RPAREN
+      }
 
       unless lparen_idx.nil? or rparen_idx.nil?
         param_tokens = object_tokens[lparen_idx..rparen_idx]
         param_tokens.each_index do |param_tokens_idx|
           this_token = param_tokens[param_tokens_idx]
           next_token = param_tokens[param_tokens_idx+1]
-          if this_token.first == :VARIABLE
-            if [:COMMA, :EQUALS, :RPAREN].include? next_token.first
-              variables_in_scope << this_token.last[:value]
+          if this_token.type == :VARIABLE
+            if [:COMMA, :EQUALS, :RPAREN].include? next_token.type
+              variables_in_scope << this_token.value
             end
           end
         end
@@ -150,20 +166,25 @@ class PuppetLint::Plugins::CheckClasses < PuppetLint::CheckPlugin
         this_token = object_tokens[object_token_idx]
         next_token = object_tokens[object_token_idx + 1]
 
-        if this_token.first == :VARIABLE
-          if next_token.first == :EQUALS
-            variables_in_scope << this_token.last[:value]
+        if this_token.type == :VARIABLE
+          if next_token.type == :EQUALS
+            variables_in_scope << this_token.value
           else
             referenced_variables << this_token
           end
         end
       end
 
+      msg = "top-scope variable being used without an explicit namespace"
       referenced_variables.each do |token|
-        unless token.last[:value].include? '::'
-          unless variables_in_scope.include? token.last[:value]
-            unless token.last[:value] =~ /\d+/
-              notify :warning, :message =>  "top-scope variable being used without an explicit namespace", :linenumber => token.last[:line]
+        unless token.value.include? '::'
+          unless variables_in_scope.include? token.value
+            unless token.value =~ /\d+/
+              notify :warning, {
+                :message    => msg,
+                :linenumber => token.line,
+                :column     => token.column,
+              }
             end
           end
         end
