357e367bac12f3c6f6cc948661dd5f436e6f96c6
Tim Sharpe <tim@sharpe.id.au>
2012-07-13 15:09:14 +1000

    Add test for #117

--- spec/puppet-lint/check_conditionals_spec.rb
+++ spec/puppet-lint/check_conditionals_spec.rb
@@ -67,4 +67,23 @@ describe PuppetLint::Plugins::CheckConditionals do
       })
     end
   end
+
+  describe 'issue-117' do
+    let(:code) { "
+      $mem = inline_template('<%
+        mem,unit = scope.lookupvar(\'::memorysize\').split
+        mem = mem.to_f
+        # Normalize mem to bytes
+        case unit
+            when nil:  mem *= (1<<0)
+            when \'kB\': mem *= (1<<10)
+            when \'MB\': mem *= (1<<20)
+            when \'GB\': mem *= (1<<30)
+            when \'TB\': mem *= (1<<40)
+        end
+        %><%= mem.to_i %>')
+    "}
+
+    its(:problems) { should == [] }
+  end
 end
