c2943447e497a80071447ca77cc97c3723f2cbfe
Tim Sharpe <tim@sharpe.id.au>
2012-10-20 10:24:15 +1100

    Load custom check plugins from other gems

--- lib/puppet-lint/plugins.rb
+++ lib/puppet-lint/plugins.rb
@@ -1,5 +1,47 @@
+require 'pathname'
+
 class PuppetLint
   class Plugins
+    # Public: Find any gems containing puppet-lint plugins and load them.
+    #
+    # Returns nothing.
+    def self.load_from_gems
+      gem_directories.select { |path|
+        (path + 'puppet-lint/plugins').directory?
+      }.each do |gem_path|
+        (gem_path + 'puppet-lint/plugins').each_child do |child|
+          if child.file? && child.extname == '.rb'
+            load child.to_path
+          end
+        end
+      end
+    end
+  private
+    # Internal: Check if RubyGems is loaded and available.
+    #
+    # Returns true if RubyGems is available, false if not.
+    def self.has_rubygems?
+      defined? ::Gem
+    end
+
+    # Internal: Retrieve a list of available gem paths from RubyGems.
+    #
+    # Returns an Array of Pathname objects.
+    def self.gem_directories
+      if has_rubygems?
+        if Gem::Specification.respond_to? :latest_specs
+          Gem::Specification.latest_specs.map do |spec|
+            Pathname.new(spec.full_gem_path) + 'lib'
+          end
+        else
+          Gem.latest_load_paths.map do |path|
+            Pathname.new(path)
+          end
+        end
+      else
+        []
+      end
+    end
   end
 end
 
@@ -11,3 +53,5 @@ require 'puppet-lint/plugins/check_strings'
 require 'puppet-lint/plugins/check_variables'
 require 'puppet-lint/plugins/check_whitespace'
 require 'puppet-lint/plugins/check_resources'
+
+PuppetLint::Plugins.load_from_gems
