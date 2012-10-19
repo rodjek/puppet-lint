f11210353ad720a0b8db3be488da3bac4ca9aaae
Tim Sharpe <tim@sharpe.id.au>
2012-10-19 18:34:22 +1100

    spec for #157

--- spec/puppet-lint/plugins/check_classes/class_parameter_defaults_spec.rb
+++ spec/puppet-lint/plugins/check_classes/class_parameter_defaults_spec.rb
@@ -6,12 +6,37 @@ describe 'class_parameter_defaults' do
 
     its(:problems) {
       should only_have_problem({
-        :kind => :warning,
-        :message => 'parameterised class parameter without a default value',
+        :kind       => :warning,
+        :message    => 'parameterised class parameter without a default value',
         :linenumber => 1,
         :column     => 11,
       })
     }
+  end
+
+  describe 'parameterised class with multiple params with a default value' do
+    let(:code) { "class foo($bar, $baz, $gronk) { }" }
+
+    its(:problems) do
+      should have_problem({
+        :kind       => :warning,
+        :message    => 'parameterised class parameter without a default value',
+        :linenumber => 1,
+        :column     => 11,
+      })
+      should have_problem({
+        :kind       => :warning,
+        :message    => 'parameterised class parameter without a default value',
+        :linenumber => 1,
+        :column     => 17,
+      })
+      should have_problem({
+        :kind       => :warning,
+        :message    => 'parameterised class parameter without a default value',
+        :linenumber => 1,
+        :column     => 23,
+      })
+    end
   end
 
   describe 'class without parameters' do
