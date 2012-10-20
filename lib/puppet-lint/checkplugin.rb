88062109262832f61b726367390fdfd7d1413cf5
Tim Sharpe <tim@sharpe.id.au>
2012-10-20 16:02:53 +1100

    TomDoc PuppetLint::CheckPlugin.check

--- lib/puppet-lint/checkplugin.rb
+++ lib/puppet-lint/checkplugin.rb
@@ -1,4 +1,10 @@
 class PuppetLint::CheckPlugin
+  # Public: Define a new lint check.
+  #
+  # name - The String name of the check.
+  # b    - The Block implementation of the check.
+  #
+  # Returns nothing.
   def self.check(name, &b)
     PuppetLint.configuration.add_check(name, &b)
   end
