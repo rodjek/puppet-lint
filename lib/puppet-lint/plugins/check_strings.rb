9e6ed953fb0c87385a263fb22386bcb9e6b2c66a
Tim Sharpe <tim@sharpe.id.au>
2013-01-28 11:42:22 -0800

    fix support for only_variable_string

--- lib/puppet-lint/plugins/check_strings.rb
+++ lib/puppet-lint/plugins/check_strings.rb
@@ -39,12 +39,6 @@ class PuppetLint::Plugins::CheckStrings < PuppetLint::CheckPlugin
         if {:VARIABLE => true, :UNENC_VARIABLE => true}.include? tokens[token_idx + 1].type
           if tokens[token_idx + 2].type == :DQPOST
             if tokens[token_idx + 2].value == ''
-              notify :warning, {
-                :message    => 'string containing only a variable',
-                :linenumber => tokens[token_idx + 1].line,
-                :column     => tokens[token_idx + 1].column,
-              }
-
               if PuppetLint.configuration.fix
                 prev_token = token.prev_token
                 prev_code_token = token.prev_code_token
@@ -55,13 +49,27 @@ class PuppetLint::Plugins::CheckStrings < PuppetLint::CheckPlugin
                 tokens.delete_at(token_idx + 2)
                 tokens.delete_at(token_idx)
 
-                prev_token.next_token = var_token
-                prev_code_token.next_code_token = var_token
-                next_code_token.prev_code_token = var_token
-                next_token.prev_token = var_token
-                var_token.type == :VARIABLE
-                var_token.value = "$#{var_token.value}" unless var_token.value.start_with? '$'
+                prev_token.next_token = var_token unless prev_token.nil?
+                prev_code_token.next_code_token = var_token unless prev_code_token.nil?
+                next_code_token.prev_code_token = var_token unless next_code_token.nil?
+                next_token.prev_token = var_token unless next_token.nil?
+                var_token.type = :VARIABLE
+                var_token.next_token = next_token
+                var_token.next_code_token = next_code_token
+                var_token.prev_code_token = prev_code_token
+                var_token.prev_token = prev_token
+                notify_type = :fixed
+                notify_token = var_token
+              else
+                notify_type = :warning
+                notify_token = tokens[token_idx + 1]
               end
+
+              notify notify_type, {
+                :message    => 'string containing only a variable',
+                :linenumber => notify_token.line,
+                :column     => notify_token.column,
+              }
             end
           end
         end
--- spec/puppet-lint/plugins/check_strings/only_variable_string_spec.rb
+++ spec/puppet-lint/plugins/check_strings/only_variable_string_spec.rb
@@ -13,4 +13,27 @@ describe 'only_variable_string' do
       })
     }
   end
+
+  describe 'string containing only a variable w/fix' do
+    before do
+      PuppetLint.configuration.fix = true
+    end
+
+    after do
+      PuppetLint.configuration.fix = false
+    end
+
+    let(:code) { '"${foo}"' }
+
+    its(:problems) {
+      should only_have_problem({
+        :kind       => :fixed,
+        :message    => 'string containing only a variable',
+        :linenumber => 1,
+        :column     => 3,
+      })
+    }
+
+    its(:manifest) { should == "$foo" }
+  end
 end
