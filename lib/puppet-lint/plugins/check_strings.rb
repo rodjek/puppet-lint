d05720b157a7ea60211be28d7280fb585f192182
Tim Sharpe <tim@sharpe.id.au>
2013-09-20 12:49:40 +0200

    Convert only_variable_string to new fix method

--- lib/puppet-lint/plugins/check_strings.rb
+++ lib/puppet-lint/plugins/check_strings.rb
@@ -28,49 +28,27 @@ end
 # a single variable only and record a warning for each instance found.
 PuppetLint.new_check(:only_variable_string) do
   def check
-    variable_tokens = Set.new [:VARIABLE, :UNENC_VARIABLE]
+    variable_token_types = Set.new [:VARIABLE, :UNENC_VARIABLE]
 
-    tokens.each_index do |token_idx|
-      token = tokens[token_idx]
-
-      if token.type == :DQPRE and token.value == ''
-        var_token = token.next_token
-        if variable_tokens.include? var_token.type
+    tokens.each_with_index do |start_token, start_token_idx|
+      if start_token.type == :DQPRE and start_token.value == ''
+        var_token = start_token.next_token
+        if variable_token_types.include? var_token.type
           eos_offset = 2
           loop do
-            eos_token = tokens[token_idx + eos_offset]
+            eos_token = tokens[start_token_idx + eos_offset]
             case eos_token.type
             when :LBRACK
               eos_offset += 3
             when :DQPOST
               if eos_token.value == ''
-                if PuppetLint.configuration.fix
-                  prev_token = token.prev_token
-                  prev_code_token = token.prev_code_token
-                  next_token = eos_token.next_token
-                  next_code_token = eos_token.next_code_token
-
-                  tokens.delete_at(token_idx + eos_offset)
-                  tokens.delete_at(token_idx)
-
-                  prev_token.next_token = var_token unless prev_token.nil?
-                  prev_code_token.next_code_token = var_token unless prev_code_token.nil?
-                  next_code_token.prev_code_token = var_token unless next_code_token.nil?
-                  next_token.prev_token = var_token unless next_token.nil?
-                  var_token.type = :VARIABLE
-                  var_token.next_token = next_token
-                  var_token.next_code_token = next_code_token
-                  var_token.prev_code_token = prev_code_token
-                  var_token.prev_token = prev_token
-                  notify_type = :fixed
-                else
-                  notify_type = :warning
-                end
-
-                notify notify_type, {
-                  :message    => 'string containing only a variable',
-                  :linenumber => var_token.line,
-                  :column     => var_token.column,
+                notify :warning, {
+                  :message     => 'string containing only a variable',
+                  :linenumber  => var_token.line,
+                  :column      => var_token.column,
+                  :start_token => start_token,
+                  :var_token   => var_token,
+                  :end_token   => eos_token,
                 }
               end
               break
@@ -81,6 +59,27 @@ PuppetLint.new_check(:only_variable_string) do
         end
       end
     end
+  end
+
+  def fix(problem)
+    prev_token = problem[:start_token].prev_token
+    prev_code_token = problem[:start_token].prev_code_token
+    next_token = problem[:end_token].next_token
+    next_code_token = problem[:end_token].next_code_token
+    var_token = problem[:var_token]
+
+    tokens.delete(problem[:start_token])
+    tokens.delete(problem[:end_token])
+
+    prev_token.next_token = var_token unless prev_token.nil?
+    prev_code_token.next_code_token = var_token unless prev_code_token.nil?
+    next_code_token.prev_code_token = var_token unless next_code_token.nil?
+    next_token.prev_token = var_token unless next_token.nil?
+    var_token.type = :VARIABLE
+    var_token.next_token = next_token
+    var_token.next_code_token = next_code_token
+    var_token.prev_code_token = prev_code_token
+    var_token.prev_token = prev_token
   end
 end
 
