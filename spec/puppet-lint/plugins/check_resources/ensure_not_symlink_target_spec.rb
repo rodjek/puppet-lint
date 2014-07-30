569322f7f28ea4446ed71075e89528f41a200231
Tim Sharpe <tim@sharpe.id.au>
2014-07-30 11:44:33 +1000

    Automatically fix ensure_not_symlink_target problems

--- spec/puppet-lint/plugins/check_resources/ensure_not_symlink_target_spec.rb
+++ spec/puppet-lint/plugins/check_resources/ensure_not_symlink_target_spec.rb
@@ -3,32 +3,87 @@ require 'spec_helper'
 describe 'ensure_not_symlink_target' do
   let(:msg) { 'symlink target specified in ensure attr' }
 
-  context 'file resource creating a symlink with seperate target attr' do
-    let(:code) { "
-      file { 'foo':
-        ensure => link,
-        target => '/foo/bar',
-      }"
-    }
+  context 'with fix disabled' do
+    context 'file resource creating a symlink with seperate target attr' do
+      let(:code) { "
+        file { 'foo':
+          ensure => link,
+          target => '/foo/bar',
+        }"
+      }
 
-    it 'should not detect any problems' do
-      expect(problems).to have(0).problems
+      it 'should not detect any problems' do
+        expect(problems).to have(0).problems
+      end
+    end
+
+    context 'file resource creating a symlink with target specified in ensure' do
+      let(:code) { "
+        file { 'foo':
+          ensure => '/foo/bar',
+        }"
+      }
+
+      it 'should only detect a single problem' do
+        expect(problems).to have(1).problem
+      end
+
+      it 'should create a warning' do
+        expect(problems).to contain_warning(msg).on_line(3).in_column(21)
+      end
     end
   end
 
-  context 'file resource creating a symlink with target specified in ensure' do
-    let(:code) { "
-      file { 'foo':
-        ensure => '/foo/bar',
-      }"
-    }
-
-    it 'should only detect a single problem' do
-      expect(problems).to have(1).problem
+  context 'with fix enabled' do
+    before do
+      PuppetLint.configuration.fix = true
     end
 
-    it 'should create a warning' do
-      expect(problems).to contain_warning(msg).on_line(3).in_column(19)
+    after do
+      PuppetLint.configuration.fix = false
+    end
+
+    context 'file resource creating a symlink with seperate target attr' do
+      let(:code) { "
+        file { 'foo':
+          ensure => link,
+          target => '/foo/bar',
+        }"
+      }
+
+      it 'should not detect any problems' do
+        expect(problems).to have(0).problems
+      end
+
+      it 'should not modify the manifest' do
+        expect(manifest).to eq(code)
+      end
+    end
+
+    context 'file resource creating a symlink with target specified in ensure' do
+      let(:code) { "
+        file { 'foo':
+          ensure => '/foo/bar',
+        }"
+      }
+      let(:fixed) { "
+        file { 'foo':
+          ensure => symlink,
+          target => '/foo/bar',
+        }"
+      }
+
+      it 'should only detect a single problem' do
+        expect(problems).to have(1).problem
+      end
+
+      it 'should fix the problem' do
+        expect(problems).to contain_fixed(msg).on_line(3).in_column(21)
+      end
+
+      it 'should create a new target param' do
+        expect(manifest).to eq(fixed)
+      end
     end
   end
 end
