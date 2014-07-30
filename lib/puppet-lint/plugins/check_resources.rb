d6785d9913ad3b3fd30d44109ef069a3f9be80cc
Tim Sharpe <tim@sharpe.id.au>
2014-07-30 11:51:01 +1000

    Clean up duplicate_params check a bit

--- lib/puppet-lint/plugins/check_resources.rb
+++ lib/puppet-lint/plugins/check_resources.rb
@@ -48,35 +48,30 @@ end
 PuppetLint.new_check(:duplicate_params) do
   def check
     resource_indexes.each do |resource|
-      resource_tokens = resource[:tokens].reject { |r|
-        formatting_tokens.include? r.type
-      }
-
       seen_params = {}
       level = 0
-      resource_tokens.each_with_index do |token, idx|
+
+      resource[:tokens].each_with_index do |token, idx|
         case token.type
         when :LBRACE
           level += 1
           next
         when :RBRACE
-          seen_params[level] = {}
+          seen_params.delete(level)
           level -= 1
           next
-        end
-        seen_params[level] ||= {}
-
-        if token.type == :FARROW
-          prev_token = resource_tokens[idx - 1]
+        when :FARROW
+          prev_token = token.prev_code_token
           next unless prev_token.type == :NAME
-          if seen_params[level].include? prev_token.value
+
+          if (seen_params[level] ||= Set.new).include?(prev_token.value)
             notify :error, {
               :message => 'duplicate parameter found in resource',
               :line    => prev_token.line,
               :column  => prev_token.column,
             }
           else
-            seen_params[level][prev_token.value] = true
+            seen_params[level] << prev_token.value
           end
         end
       end
