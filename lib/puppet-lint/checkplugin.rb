3dab36de529adf8179371794f2bf8da4dfef816f
Tim Sharpe <tim@sharpe.id.au>
2014-08-17 21:16:28 +1000

    Document PuppetLint::CheckPlugin

--- lib/puppet-lint/checkplugin.rb
+++ lib/puppet-lint/checkplugin.rb
@@ -1,8 +1,22 @@
+# Public: A class that contains and provides information for the puppet-lint
+# checks.
+#
+# This class should not be used directly, but instead should be inherited.
+#
+# Examples
+#
+#   class PuppetLint::Plugin::CheckFoo < PuppetLint::CheckPlugin
+#   end
 class PuppetLint::CheckPlugin
+  # Internal: Initialise a new PuppetLint::CheckPlugin.
   def initialize
     @problems = []
   end
 
+  # Internal: Check the manifest for problems and filter out any problems that
+  # should be ignored.
+  #
+  # Returns an Array of problem Hashes.
   def run
     check
 
@@ -17,6 +31,9 @@ class PuppetLint::CheckPlugin
     @problems
   end
 
+  # Internal: Fix any problems the check plugin has detected.
+  #
+  # Returns an Array of problem Hashes.
   def fix_problems
     @problems.each do |problem|
       if self.respond_to?(:fix)
@@ -35,46 +52,84 @@ class PuppetLint::CheckPlugin
 
   private
 
+  # Public: Provides the tokenised manifest to the check plugins.
+  #
+  # Returns an Array of PuppetLint::Lexer::Token objects.
   def tokens
     PuppetLint::Data.tokens
   end
 
+  # Public: Provides the resource titles to the check plugins.
+  #
+  # Returns an Array of PuppetLint::Lexer::Token objects.
   def title_tokens
     PuppetLint::Data.title_tokens
   end
 
+  # Public: Provides positional information for any resource declarations in
+  # the tokens array to the check plugins.
+  #
+  # Returns an Array of Hashes containing the position information.
   def resource_indexes
     PuppetLint::Data.resource_indexes
   end
 
+  # Public: Provides positional information for any class definitions in the
+  # tokens array to the check plugins.
+  #
+  # Returns an Array of Hashes containing the position information.
   def class_indexes
     PuppetLint::Data.class_indexes
   end
 
+  # Public: Provides positional information for any defined type definitions in
+  # the tokens array to the check plugins.
+  #
+  # Returns an Array of Hashes containing the position information.
   def defined_type_indexes
     PuppetLint::Data.defined_type_indexes
   end
 
+  # Public: Provides the expanded path of the file being analysed to check
+  # plugins.
+  #
+  # Returns the String path.
   def fullpath
     PuppetLint::Data.fullpath
   end
 
+  # Public: Provides the path of the file being analysed as it was provided to
+  # puppet-lint to the check plugins.
+  #
+  # Returns the String path.
   def path
     PuppetLint::Data.path
   end
 
+  # Public: Provides the name of the file being analysed to the check plugins.
+  #
+  # Returns the String file name.
   def filename
     PuppetLint::Data.filename
   end
 
+  # Public: Provides a list of formatting tokens to the check plugins.
+  #
+  # Returns an Array of Symbol token types.
   def formatting_tokens
     PuppetLint::Data.formatting_tokens
   end
 
+  # Public: Provides a list of manifest lines to the check plugins.
+  #
+  # Returns an Array of manifest lines.
   def manifest_lines
     PuppetLint::Data.manifest_lines
   end
 
+  # Internal: Prepare default problem report information.
+  #
+  # Returns a Hash of default problem information.
   def default_info
     @default_info ||= {
       :check      => self.class.const_get('NAME'),
