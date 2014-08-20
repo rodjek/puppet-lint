rodjek/puppet-lint

commit 91fd43794aafefa8ead8411ea34633aad24f648b

Only ignore regex match vars, not all vars containing numbers

lib/puppet-lint/plugins/check_classes.rb
@@ -221,7 +221,7 @@ PuppetLint.new_check(:variable_scope) do
       referenced_variables.each do |token|
         unless token.value.include? '::'
           unless variables_in_scope.include? token.value
-            unless token.value =~ /\d+/
+            unless token.value =~ /\A\d+\Z/
               notify :warning, {
                 :message => msg,
                 :line    => token.line,

commit 0128b97b6f284ce2ad758290896aa9e231bd9aba

[#291] Ignore index braces for scope variables
So that we just check the name of the variable against `variables_in_scope`.
For example `$foo` instead of `$foo[0]` or `$foo['bar']`. It won't do
anything to empty or unclosed braces.

This fixes the two tests introduced in 3ec6651 for accessing indexes of
array and hash variables. It looks like arrays were a regression introduced
in 91fd437, but hashes may not have worked previously.

lib/puppet-lint/plugins/check_classes.rb
@@ -256,7 +256,7 @@ PuppetLint.new_check(:variable_scope) do
         end
 
         unless token.value.include? '::'
-          unless variables_in_scope.include? token.value
+          unless variables_in_scope.include? token.value.gsub(/\[.+\]\Z/, '')
             unless token.value =~ /\A\d+\Z/
               notify :warning, {
                 :message => msg,