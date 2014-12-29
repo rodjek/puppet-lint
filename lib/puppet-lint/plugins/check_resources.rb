1b22dc5a73f52ff443e1c27d64db02774b1ab7f2
Mathieu Parent <Mathieu.PARENT@nantesmetropole.fr>
2014-12-29 17:45:49 +0100

    Add fix to "ensure found on line but it's not the first attribute"

--- lib/puppet-lint/plugins/check_resources.rb
+++ lib/puppet-lint/plugins/check_resources.rb
@@ -34,12 +34,77 @@ PuppetLint.new_check(:ensure_first_param) do
         if ensure_attr_index > 0
           ensure_token = resource[:param_tokens][ensure_attr_index]
           notify :warning, {
-            :message => "ensure found on line but it's not the first attribute",
-            :line    => ensure_token.line,
-            :column  => ensure_token.column,
+            :message  => "ensure found on line but it's not the first attribute",
+            :line     => ensure_token.line,
+            :column   => ensure_token.column,
+            :resource => resource,
           }
         end
       end
+    end
+  end
+  def fix(problem)
+    # We find the first and ensure paramss boundaries
+    first_param_name_token = nil
+    first_param_name_idx = nil
+    first_param_comma_token = nil
+    first_param_comma_idx = nil
+    ensure_param_name_token = nil
+    ensure_param_name_idx = nil
+    ensure_param_comma_token = nil
+    ensure_param_comma_idx = nil
+    tokens[(problem[:resource][:start])..(problem[:resource][:end])].each_with_index do |token, token_idx|
+      if first_param_name_token.nil?
+        if token.type == :NAME
+          first_param_name_token = token
+          first_param_name_idx = problem[:resource][:start] + token_idx
+        end
+      elsif first_param_comma_token.nil?
+        if token.type == :COMMA
+          first_param_comma_token = token
+          first_param_comma_idx = problem[:resource][:start] + token_idx
+        end
+      elsif ensure_param_name_token.nil?
+        if token.type == :NAME and token.value == 'ensure'
+          ensure_param_name_token = token
+          ensure_param_name_idx = problem[:resource][:start] + token_idx
+        end
+      elsif ensure_param_comma_token.nil?
+        if token.type == :COMMA or token.type == :SEMIC
+          ensure_param_comma_token = token
+          ensure_param_comma_idx = problem[:resource][:start] + token_idx
+          break
+        end
+      end
+    end
+    if first_param_name_token.nil? or first_param_comma_token.nil? or ensure_param_name_token.nil? or ensure_param_comma_token.nil?
+      raise PuppetLint::NoFix
+    end
+    # Flip params
+    prev_token = first_param_name_token.prev_token
+    first_param_name_token.prev_token = ensure_param_name_token.prev_token
+    ensure_param_name_token.prev_token = prev_token
+
+    prev_code_token = first_param_name_token.prev_code_token
+    first_param_name_token.prev_code_token = ensure_param_name_token.prev_code_token
+    ensure_param_name_token.prev_code_token = prev_code_token
+
+    next_token = first_param_comma_token.next_token
+    first_param_comma_token = ensure_param_comma_token.next_token
+    ensure_param_comma_token.next_token = next_token
+
+    next_code_token = first_param_comma_token.next_code_token
+    first_param_comma_code_token = ensure_param_comma_token.next_code_token
+    ensure_param_comma_token.next_code_token = next_code_token
+
+    # Update index
+    ensure_tmp = tokens.slice!(ensure_param_name_idx..ensure_param_comma_idx-1)
+    first_tmp = tokens.slice!(first_param_name_idx..first_param_comma_idx-1)
+    ensure_tmp.reverse_each do |item|
+      tokens.insert(first_param_name_idx, item)
+    end
+    first_tmp.reverse_each do |item|
+      tokens.insert(ensure_param_name_idx + ensure_tmp.length - first_tmp.length, item)
     end
   end
 end
