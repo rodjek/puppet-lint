8eac6c69ec5be7db2eea5724b85eec325e5a82b3
Derek Olsen <derek.olsen@jivesoftware.com>
2015-02-03 15:11:30 -0800

    Add the fix functionality to puppet_url_without_modules This is intended to fix issue #390

--- spec/puppet-lint/plugins/check_strings/puppet_url_without_modules_spec.rb
+++ spec/puppet-lint/plugins/check_strings/puppet_url_without_modules_spec.rb
@@ -11,15 +11,43 @@ describe 'puppet_url_without_modules' do
     end
   end
 
-  context 'puppet:// url without modules' do
-    let(:code) { "'puppet:///foo'" }
+  context 'with fix disabled' do
+    context 'puppet:// url without modules' do
+      let(:code) { "'puppet:///foo'" }
 
-    it 'should only detect a single problem' do
-      expect(problems).to have(1).problem
+      it 'should only detect a single problem' do
+        expect(problems).to have(1).problem
+      end
+
+      it 'should create a warning' do
+        expect(problems).to contain_warning(msg).on_line(1).in_column(1)
+      end
+    end
+  end
+
+  context 'with fix enabled' do
+    before do
+      PuppetLint.configuration.fix = true
     end
 
-    it 'should create a warning' do
-      expect(problems).to contain_warning(msg).on_line(1).in_column(1)
+    after do
+      PuppetLint.configuration.fix = false
+    end
+
+    context 'puppet:// url without modules' do
+      let(:code) { "'puppet:///foo'" }
+
+      it 'should only detect a single problem' do
+        expect(problems).to have(1).problem
+      end
+
+      it 'should fix the manifest' do
+        expect(problems).to contain_fixed(msg).on_line(1).in_column(1)
+      end
+
+      it 'should insert modules into the path' do
+        expect(manifest).to eq("'puppet:///modules/foo'")
+      end
     end
   end
 end
