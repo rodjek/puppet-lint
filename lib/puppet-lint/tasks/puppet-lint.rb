4ab65ffabdc2c298bbb71efd94520224b53ab890
Kasumi Hanazuki <kasumi@rollingapple.net>
2016-03-22 11:35:15 +0900

    Support automatic fixing in Rake task as described in README

--- lib/puppet-lint/tasks/puppet-lint.rb
+++ lib/puppet-lint/tasks/puppet-lint.rb
@@ -78,6 +78,10 @@ class PuppetLint
             linter.file = puppet_file
             linter.run
             linter.print_problems
+
+            if PuppetLint.configuration.fix && !linter.problems.empty?
+              IO.write(puppet_file, linter.manifest)
+            end
           end
           abort if linter.errors? || (
             linter.warnings? && PuppetLint.configuration.fail_on_warnings
