d25a229d17ff5ac41474398e158d5c2f5af6189f
Reid 'arrdem' McKenzie <rmckenzie@twitter.com>
2016-12-13 14:32:05 -0800

    Refactor token accessors to use custom getters/setters
    This patch rewrites {prev,next}(_code)?_token so that they should be more repeatable and will
    preserve the structure invariants in the tokens linked list.
    
    Because {prev,next}_code_token are just views on {prev,next}_token, they no longer have setters and
    are implemented as linked list traversals on the corresponding token link.
    
    Because users can't be trusted to set up links in the list, the {prev,next}_token= setters now
    automatically fix the prev and next links of all tokens involved. This should make splicing new
    tokens in easier.
    
    Note - this patch does not provide automatic propagation of changes to line and column number. Those
    things still need to be implemented. Probably by some push method.

--- lib/puppet-lint/lexer/token.rb
+++ lib/puppet-lint/lexer/token.rb
@@ -1,7 +1,7 @@
 class PuppetLint
   class Lexer
-    # Public: Stores a fragment of the manifest and the information about its
-    # location in the manifest.
+    # Public: Stores a fragment of the manifest and the information about its location in the
+    # manifest.
     class Token
       # Public: Returns the Symbol type of the Token.
       attr_accessor :type
@@ -12,36 +12,61 @@ class PuppetLint
       # Public: Returns the raw value of the Token.
       attr_accessor :raw
 
-      # Public: Returns the Integer line number of the manifest text where
-      # the Token can be found.
+      # Public: Returns the Integer line number of the manifest text where the Token can be found.
       attr_reader :line
 
-      # Public: Returns the Integer column number of the line of the manifest
-      # text where the Token can be found.
+      # Public: Returns the Integer column number of the line of the manifest text where the Token
+      # can be found.
       attr_reader :column
 
       # Public: Gets/sets the next token in the manifest.
-      attr_accessor :next_token
+      attr_reader :next_token
+
+      def next_token=(val)
+        if(val != @next_token) then
+          t = @next_token
+          @next_token = val
+          val.prev_token = self
+          val.next_token = t
+        end
+      end
 
       # Public: Gets/sets the previous token in the manifest.
-      attr_accessor :prev_token
+      attr_reader :prev_token
 
-      # Public: Gets/sets the next code token (skips whitespace, comments,
-      # etc) in the manifest.
-      attr_accessor :next_code_token
+      def prev_token=(val)
+        if(val != @prev_token)
+          t = @prev_token
+          @prev_token = val
+          t.next_token = val
+        end
+      end
 
-      # Public: Gets/sets the previous code tokne (skips whitespace,
-      # comments, etc) in the manifest.
-      attr_accessor :prev_code_token
+      # Public: Gets the next code token (skips whitespace, comments, etc) in the manifest.
+      def next_code_token
+        t = @next_token
+        while t and t.whitespace?
+          t = t.next_token
+        end
+        return t
+      end
+
+      # Public: Gets the previous code token (skips whitespace, comments, etc) in the manifest.
+      def prev_code_token
+        t = @prev_token
+        while t and t.whitespace?
+          t = t.prev_token
+        end
+        return t
+      end
 
       # Public: Initialise a new Token object.
       #
       # type   - An upper case Symbol describing the type of Token.
       # value  - The String value of the Token.
-      # line   - The Integer line number where the Token can be found in the
-      #          manifest.
-      # column - The Integer number of characters from the start of the line to
-      #          the start of the Token.
+      # line   - The Integer line number where the Token can be found in the manifest.
+      # column - The Integer number of characters from the start of the line to the start of the
+      #          Token.
       #
       # Returns the instantiated Token.
       def initialize(type, value, line, column)
@@ -55,8 +80,7 @@ class PuppetLint
         @prev_code_token = nil
       end
 
-      # Public: Produce a human friendly description of the Token when
-      # inspected.
+      # Public: Produce a human friendly description of the Token when inspected.
       #
       # Returns a String describing the Token.
       def inspect
@@ -95,6 +119,10 @@ class PuppetLint
         else
           @value
         end
+      end
+
+      def whitespace?
+        return PuppetLint::Lexer.FORMATTING_TOKENS.fetch(@type, false)
       end
     end
   end
