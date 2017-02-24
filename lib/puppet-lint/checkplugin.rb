a4900093982d7efa29d2ba55b61f10ae85145af2
Tim Sharpe <tim@sharpe.id.au>
2017-02-24 12:27:15 -0800

    Never attempt to fix syntax errors

--- lib/puppet-lint/checkplugin.rb
+++ lib/puppet-lint/checkplugin.rb
@@ -35,7 +35,7 @@ class PuppetLint::CheckPlugin
   #
   # Returns an Array of problem Hashes.
   def fix_problems
-    @problems.reject { |problem| problem[:kind] == :ignored }.each do |problem|
+    @problems.reject { |problem| problem[:kind] == :ignored || problem[:check] == :syntax }.each do |problem|
       if self.respond_to?(:fix)
         begin
           fix(problem)
