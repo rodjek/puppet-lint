b174edef1e6aff74df6fe69ed5c86885c0bbde82
Kevin Paulisse <kpaulisse@users.noreply.github.com>
2017-07-06 14:49:30 -0500

    Revert "Always load the prerelease plugins"
    This reverts commit 08952e7e07865a498125c011ca3e3473548b1509.

--- lib/puppet-lint/plugins.rb
+++ lib/puppet-lint/plugins.rb
@@ -40,9 +40,11 @@ class PuppetLint
     # Returns an Array of Gem::Specification objects.
     def self.gemspecs
       @gemspecs ||= if Gem::Specification.respond_to?(:latest_specs)
-        # The 'true' allows it to load prerelease plugins (which ruby defines as
-        # any gem which has a letter in its version number).
-        Gem::Specification.latest_specs(true)
+        # Environment variable to load prerelease plugins (which ruby defines as
+        # any gem which has a letter in its version number). Can't use puppet-lint configuration
+        # object here because this code executes before the command line is parsed.
+        load_prerelease_plugins = ENV.key?('PUPPET_LINT_LOAD_PRERELEASE_PLUGINS')
+        Gem::Specification.latest_specs(load_prerelease_plugins)
       else
         Gem.searcher.init_gemspecs
       end
