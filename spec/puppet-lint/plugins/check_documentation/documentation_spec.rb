cb2b40e469ca019911d8980e9cc1f53191a89de7
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2020-10-02 14:14:41 +0200

    Disallow empty lines between docs and class/define
    puppet-strings doesn't parse documentation if there is an empty line
    between the comments and the actual class/define. This enhances the
    check by only accepting a single newline.

--- spec/puppet-lint/plugins/check_documentation/documentation_spec.rb
+++ spec/puppet-lint/plugins/check_documentation/documentation_spec.rb
@@ -29,6 +29,24 @@ describe 'documentation' do
     end
   end
 
+  describe 'incorrectly documented class' do
+    let(:code) do
+      <<-END
+        # foo
+
+        class test {}
+      END
+    end
+
+    it 'should only detect a single problem' do
+      expect(problems).to have(1).problem
+    end
+
+    it 'should create a warning' do
+      expect(problems).to contain_warning(class_msg).on_line(3).in_column(9)
+    end
+  end
+
   describe 'undocumented defined type' do
     let(:code) { 'define test {}' }
 
