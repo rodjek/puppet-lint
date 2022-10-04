d4293872486e3a18d4513e7ae74732b79a2005bc
Craig Gumbley <craiggumbley@gmail.com>
2022-10-04 12:49:59 +0100

    (MAINT) Rubocop fixes for lexer

--- lib/puppet-lint/lexer/string_slurper.rb
+++ lib/puppet-lint/lexer/string_slurper.rb
@@ -1,173 +1,171 @@
 require 'strscan'
 
-class PuppetLint
-  class Lexer
-    # Document this
-    # TODO
-    class StringSlurper
-      class UnterminatedStringError < StandardError; end
+class PuppetLint::Lexer
+  # Internal: A class for slurping strings from a Puppet manifest.
+  class StringSlurper
+    attr_accessor :scanner
+    attr_accessor :results
+    attr_accessor :interp_stack
 
-      attr_accessor :scanner
-      attr_accessor :results
-      attr_accessor :interp_stack
+    START_INTERP_PATTERN = %r{\$\{}.freeze
+    END_INTERP_PATTERN = %r{\}}.freeze
+    END_STRING_PATTERN = %r{(\A|[^\\])(\\\\)*"}.freeze
+    UNENC_VAR_PATTERN = %r{(\A|[^\\])\$(::)?(\w+(-\w+)*::)*\w+(-\w+)*}.freeze
+    ESC_DQUOTE_PATTERN = %r{\\+"}.freeze
+    LBRACE_PATTERN = %r{\{}.freeze
 
-      START_INTERP_PATTERN = %r{\$\{}.freeze
-      END_INTERP_PATTERN = %r{\}}.freeze
-      END_STRING_PATTERN = %r{(\A|[^\\])(\\\\)*"}.freeze
-      UNENC_VAR_PATTERN = %r{(\A|[^\\])\$(::)?(\w+(-\w+)*::)*\w+(-\w+)*}.freeze
-      ESC_DQUOTE_PATTERN = %r{\\+"}.freeze
-      LBRACE_PATTERN = %r{\{}.freeze
+    def initialize(string)
+      @scanner = StringScanner.new(string)
+      @results = []
+      @interp_stack = []
+      @segment = []
+    end
 
-      def initialize(string)
-        @scanner = StringScanner.new(string)
-        @results = []
-        @interp_stack = []
-        @segment = []
-      end
+    def parse
+      @segment_type = :STRING
 
-      def parse
-        @segment_type = :STRING
-
-        until scanner.eos?
-          if scanner.match?(START_INTERP_PATTERN)
-            start_interp
-          elsif !interp_stack.empty? && scanner.match?(LBRACE_PATTERN)
-            read_char
-          elsif scanner.match?(END_INTERP_PATTERN)
-            end_interp
-          elsif unenclosed_variable?
-            unenclosed_variable
-          elsif scanner.match?(END_STRING_PATTERN)
-            end_string
-            break if interp_stack.empty?
-          elsif scanner.match?(ESC_DQUOTE_PATTERN)
-            @segment << scanner.scan(ESC_DQUOTE_PATTERN)
-          else
-            read_char
-          end
-        end
-
-        raise UnterminatedStringError if results.empty? && scanner.matched?
-
-        results
-      end
-
-      def unenclosed_variable?
-        interp_stack.empty? &&
-          scanner.match?(UNENC_VAR_PATTERN) &&
-          (@segment.last.nil? ? true : !@segment.last.end_with?('\\'))
-      end
-
-      def parse_heredoc(heredoc_tag)
-        heredoc_name = heredoc_tag[%r{\A"?(.+?)"?(:.+?)?#{PuppetLint::Lexer::WHITESPACE_RE}*(/.*)?\Z}o, 1]
-        end_heredoc_pattern = %r{^\|?\s*-?\s*#{Regexp.escape(heredoc_name)}$}
-        interpolation = heredoc_tag.start_with?('"')
-
-        @segment_type = :HEREDOC
-
-        until scanner.eos?
-          if scanner.match?(end_heredoc_pattern)
-            end_heredoc(end_heredoc_pattern)
-            break if interp_stack.empty?
-          elsif interpolation && scanner.match?(START_INTERP_PATTERN)
-            start_interp
-          elsif interpolation && !interp_stack.empty? && scanner.match?(LBRACE_PATTERN)
-            read_char
-          elsif interpolation && unenclosed_variable?
-            unenclosed_variable
-          elsif interpolation && scanner.match?(END_INTERP_PATTERN)
-            end_interp
-          else
-            read_char
-          end
-        end
-
-        results
-      end
-
-      def read_char
-        @segment << scanner.getch
-
-        return if interp_stack.empty?
-
-        case @segment.last
-        when '{'
-          interp_stack.push(true)
-        when '}'
-          interp_stack.pop
-        end
-      end
-
-      # Get the number of characters consumed by the StringSlurper.
-      #
-      # StringScanner from Ruby 2.0 onwards supports #charpos which returns
-      # the number of characters and is multibyte character aware.
-      #
-      # Prior to this, Ruby's multibyte character support in Strings was a
-      # bit unusual and neither String#length nor String#split behave as
-      # expected, so we use String#scan to split all the consumed text using
-      # a UTF-8 aware regex and use the length of the result
-      def consumed_chars
-        return scanner.charpos if scanner.respond_to?(:charpos)
-
-        (scanner.pre_match + scanner.matched).scan(%r{.}mu).length
-      end
-
-      def start_interp
-        if @segment.last && @segment.last == '\\'
+      until scanner.eos?
+        if scanner.match?(START_INTERP_PATTERN)
+          start_interp
+        elsif !interp_stack.empty? && scanner.match?(LBRACE_PATTERN)
           read_char
-          return
-        end
-
-        if interp_stack.empty?
-          scanner.skip(START_INTERP_PATTERN)
-          results << [@segment_type, @segment.join]
-          @segment = []
+        elsif scanner.match?(END_INTERP_PATTERN)
+          end_interp
+        elsif unenclosed_variable?
+          unenclosed_variable
+        elsif scanner.match?(END_STRING_PATTERN)
+          end_string
+          break if interp_stack.empty?
+        elsif scanner.match?(ESC_DQUOTE_PATTERN)
+          @segment << scanner.scan(ESC_DQUOTE_PATTERN)
         else
-          @segment << scanner.scan(START_INTERP_PATTERN)
+          read_char
         end
+      end
 
+      raise UnterminatedStringError if results.empty? && scanner.matched?
+
+      results
+    end
+
+    def unenclosed_variable?
+      interp_stack.empty? &&
+        scanner.match?(UNENC_VAR_PATTERN) &&
+        (@segment.last.nil? ? true : !@segment.last.end_with?('\\'))
+    end
+
+    def parse_heredoc(heredoc_tag)
+      heredoc_name = heredoc_tag[%r{\A"?(.+?)"?(:.+?)?#{PuppetLint::Lexer::WHITESPACE_RE}*(/.*)?\Z}o, 1]
+      end_heredoc_pattern = %r{^\|?\s*-?\s*#{Regexp.escape(heredoc_name)}$}
+      interpolation = heredoc_tag.start_with?('"')
+
+      @segment_type = :HEREDOC
+
+      until scanner.eos?
+        if scanner.match?(end_heredoc_pattern)
+          end_heredoc(end_heredoc_pattern)
+          break if interp_stack.empty?
+        elsif interpolation && scanner.match?(START_INTERP_PATTERN)
+          start_interp
+        elsif interpolation && !interp_stack.empty? && scanner.match?(LBRACE_PATTERN)
+          read_char
+        elsif interpolation && unenclosed_variable?
+          unenclosed_variable
+        elsif interpolation && scanner.match?(END_INTERP_PATTERN)
+          end_interp
+        else
+          read_char
+        end
+      end
+
+      results
+    end
+
+    def read_char
+      @segment << scanner.getch
+
+      return if interp_stack.empty?
+
+      case @segment.last
+      when '{'
         interp_stack.push(true)
+      when '}'
+        interp_stack.pop
+      end
+    end
+
+    # Get the number of characters consumed by the StringSlurper.
+    #
+    # StringScanner from Ruby 2.0 onwards supports #charpos which returns
+    # the number of characters and is multibyte character aware.
+    #
+    # Prior to this, Ruby's multibyte character support in Strings was a
+    # bit unusual and neither String#length nor String#split behave as
+    # expected, so we use String#scan to split all the consumed text using
+    # a UTF-8 aware regex and use the length of the result
+    def consumed_chars
+      return scanner.charpos if scanner.respond_to?(:charpos)
+
+      (scanner.pre_match + scanner.matched).scan(%r{.}mu).length
+    end
+
+    def start_interp
+      if @segment.last && @segment.last == '\\'
+        read_char
+        return
       end
 
-      def end_interp
-        if interp_stack.empty?
-          @segment << scanner.scan(END_INTERP_PATTERN)
-          return
-        else
-          interp_stack.pop
-        end
-
-        if interp_stack.empty?
-          results << [:INTERP, @segment.join]
-          @segment = []
-          scanner.skip(END_INTERP_PATTERN)
-        else
-          @segment << scanner.scan(END_INTERP_PATTERN)
-        end
-      end
-
-      def unenclosed_variable
-        read_char if scanner.match?(%r{.\$})
-
+      if interp_stack.empty?
+        scanner.skip(START_INTERP_PATTERN)
         results << [@segment_type, @segment.join]
-        results << [:UNENC_VAR, scanner.scan(UNENC_VAR_PATTERN)]
         @segment = []
+      else
+        @segment << scanner.scan(START_INTERP_PATTERN)
       end
 
-      def end_heredoc(pattern)
-        results << [:HEREDOC, @segment.join]
-        results << [:HEREDOC_TERM, scanner.scan(pattern)]
+      interp_stack.push(true)
+    end
+
+    def end_interp
+      if interp_stack.empty?
+        @segment << scanner.scan(END_INTERP_PATTERN)
+        return
+      else
+        interp_stack.pop
       end
 
-      def end_string
-        if interp_stack.empty?
-          @segment << scanner.scan(END_STRING_PATTERN).gsub!(%r{"\Z}, '')
-          results << [@segment_type, @segment.join]
-        else
-          @segment << scanner.scan(END_STRING_PATTERN)
-        end
+      if interp_stack.empty?
+        results << [:INTERP, @segment.join]
+        @segment = []
+        scanner.skip(END_INTERP_PATTERN)
+      else
+        @segment << scanner.scan(END_INTERP_PATTERN)
+      end
+    end
+
+    def unenclosed_variable
+      read_char if scanner.match?(%r{.\$})
+
+      results << [@segment_type, @segment.join]
+      results << [:UNENC_VAR, scanner.scan(UNENC_VAR_PATTERN)]
+      @segment = []
+    end
+
+    def end_heredoc(pattern)
+      results << [:HEREDOC, @segment.join]
+      results << [:HEREDOC_TERM, scanner.scan(pattern)]
+    end
+
+    def end_string
+      if interp_stack.empty?
+        @segment << scanner.scan(END_STRING_PATTERN).gsub!(%r{"\Z}, '')
+        results << [@segment_type, @segment.join]
+      else
+        @segment << scanner.scan(END_STRING_PATTERN)
       end
     end
   end
 end
+
+# Internal: An error thrown when an unterminated string is encountered.
+class PuppetLint::Lexer::StringSlurper::UnterminatedStringError < StandardError; end
--- lib/puppet-lint/lexer/token.rb
+++ lib/puppet-lint/lexer/token.rb
@@ -1,210 +1,208 @@
-class PuppetLint
-  class Lexer
-    # Public: Stores a fragment of the manifest and the information about its
-    # location in the manifest.
-    class Token
-      # Public: Returns the Symbol type of the Token.
-      attr_accessor :type
+class PuppetLint::Lexer
+  # Public: Stores a fragment of the manifest and the information about its
+  # location in the manifest.
+  class Token
+    # Public: Returns the Symbol type of the Token.
+    attr_accessor :type
 
-      # Public: Returns the String value of the Token.
-      attr_accessor :value
+    # Public: Returns the String value of the Token.
+    attr_accessor :value
 
-      # Public: Returns the raw value of the Token.
-      attr_accessor :raw
+    # Public: Returns the raw value of the Token.
+    attr_accessor :raw
 
-      # Public: Returns the Integer line number of the manifest text where
-      # the Token can be found.
-      attr_reader :line
+    # Public: Returns the Integer line number of the manifest text where
+    # the Token can be found.
+    attr_reader :line
 
-      # Public: Returns the Integer column number of the line of the manifest
-      # text where the Token can be found.
-      attr_reader :column
+    # Public: Returns the Integer column number of the line of the manifest
+    # text where the Token can be found.
+    attr_reader :column
 
-      # Public: Gets/sets the next token in the manifest.
-      attr_accessor :next_token
+    # Public: Gets/sets the next token in the manifest.
+    attr_accessor :next_token
 
-      # Public: Gets/sets the previous token in the manifest.
-      attr_accessor :prev_token
+    # Public: Gets/sets the previous token in the manifest.
+    attr_accessor :prev_token
 
-      # Public: Gets/sets the next code token (skips whitespace, comments,
-      # etc) in the manifest.
-      attr_accessor :next_code_token
+    # Public: Gets/sets the next code token (skips whitespace, comments,
+    # etc) in the manifest.
+    attr_accessor :next_code_token
 
-      # Public: Gets/sets the previous code tokne (skips whitespace,
-      # comments, etc) in the manifest.
-      attr_accessor :prev_code_token
+    # Public: Gets/sets the previous code tokne (skips whitespace,
+    # comments, etc) in the manifest.
+    attr_accessor :prev_code_token
 
-      # Public: Initialise a new Token object.
-      #
-      # type   - An upper case Symbol describing the type of Token.
-      # value  - The String value of the Token.
-      # line   - The Integer line number where the Token can be found in the
-      #          manifest.
-      # column - The Integer number of characters from the start of the line to
-      #          the start of the Token.
-      #
-      # Returns the instantiated Token.
-      def initialize(type, value, line, column)
-        @value = value
-        @type = type
-        @line = line
-        @column = column
-        @next_token = nil
-        @prev_token = nil
-        @next_code_token = nil
-        @prev_code_token = nil
-      end
+    # Public: Initialise a new Token object.
+    #
+    # type   - An upper case Symbol describing the type of Token.
+    # value  - The String value of the Token.
+    # line   - The Integer line number where the Token can be found in the
+    #          manifest.
+    # column - The Integer number of characters from the start of the line to
+    #          the start of the Token.
+    #
+    # Returns the instantiated Token.
+    def initialize(type, value, line, column)
+      @value = value
+      @type = type
+      @line = line
+      @column = column
+      @next_token = nil
+      @prev_token = nil
+      @next_code_token = nil
+      @prev_code_token = nil
+    end
 
-      # Public: Produce a human friendly description of the Token when
-      # inspected.
-      #
-      # Returns a String describing the Token.
-      def inspect
-        "<Token #{@type.inspect} (#{@value}) @#{@line}:#{@column}>"
-      end
+    # Public: Produce a human friendly description of the Token when
+    # inspected.
+    #
+    # Returns a String describing the Token.
+    def inspect
+      "<Token #{@type.inspect} (#{@value}) @#{@line}:#{@column}>"
+    end
 
-      # Public: Produce a Puppet DSL representation of a Token.
-      #
-      # Returns a Puppet DSL String.
-      def to_manifest
-        case @type
-        when :STRING
-          "\"#{@value}\""
-        when :SSTRING
-          "'#{@value}'"
-        when :DQPRE
-          "\"#{@value}#{string_suffix}"
-        when :DQPOST
-          "#{string_prefix}#{@value}\""
-        when :DQMID
-          "#{string_prefix}#{@value}#{string_suffix}"
-        when :VARIABLE
-          enclose_token_types = Set[:DQPRE, :DQMID, :HEREDOC_PRE, :HEREDOC_MID].freeze
-          if !@prev_code_token.nil? && enclose_token_types.include?(@prev_code_token.type)
-            @raw.nil? ? @value : @raw
-          else
-            "$#{@value}"
-          end
-        when :UNENC_VARIABLE
+    # Public: Produce a Puppet DSL representation of a Token.
+    #
+    # Returns a Puppet DSL String.
+    def to_manifest
+      case @type
+      when :STRING
+        "\"#{@value}\""
+      when :SSTRING
+        "'#{@value}'"
+      when :DQPRE
+        "\"#{@value}#{string_suffix}"
+      when :DQPOST
+        "#{string_prefix}#{@value}\""
+      when :DQMID
+        "#{string_prefix}#{@value}#{string_suffix}"
+      when :VARIABLE
+        enclose_token_types = Set[:DQPRE, :DQMID, :HEREDOC_PRE, :HEREDOC_MID].freeze
+        if !@prev_code_token.nil? && enclose_token_types.include?(@prev_code_token.type)
+          @raw.nil? ? @value : @raw
+        else
           "$#{@value}"
-        when :NEWLINE
-          "\n"
-        when :COMMENT
-          "##{@value}"
-        when :REGEX
-          "/#{@value}/"
-        when :MLCOMMENT
-          @raw
-        when :HEREDOC_OPEN
-          "@(#{@value})"
-        when :HEREDOC
-          @raw
-        when :HEREDOC_PRE
-          "#{@value}#{string_suffix}"
-        when :HEREDOC_POST
-          "#{string_prefix}#{@raw}"
-        when :HEREDOC_MID
-          "#{string_prefix}#{@value}#{string_suffix}"
-        else
-          @value
         end
+      when :UNENC_VARIABLE
+        "$#{@value}"
+      when :NEWLINE
+        "\n"
+      when :COMMENT
+        "##{@value}"
+      when :REGEX
+        "/#{@value}/"
+      when :MLCOMMENT
+        @raw
+      when :HEREDOC_OPEN
+        "@(#{@value})"
+      when :HEREDOC
+        @raw
+      when :HEREDOC_PRE
+        "#{@value}#{string_suffix}"
+      when :HEREDOC_POST
+        "#{string_prefix}#{@raw}"
+      when :HEREDOC_MID
+        "#{string_prefix}#{@value}#{string_suffix}"
+      else
+        @value
       end
+    end
 
-      def string_suffix
-        no_enclose_tokens = Set.new([:UNENC_VARIABLE, :DQMID, :DQPOST, :HEREDOC_MID, :HEREDOC_POST])
-        if next_token && no_enclose_tokens.include?(next_token.type)
-          ''
-        else
-          '${'
+    def string_suffix
+      no_enclose_tokens = Set.new([:UNENC_VARIABLE, :DQMID, :DQPOST, :HEREDOC_MID, :HEREDOC_POST])
+      if next_token && no_enclose_tokens.include?(next_token.type)
+        ''
+      else
+        '${'
+      end
+    end
+
+    def string_prefix
+      no_enclose_tokens = Set.new([:UNENC_VARIABLE, :DQPRE, :DQMID, :HEREDOC_PRE, :HEREDOC_MID])
+      if prev_token && no_enclose_tokens.include?(prev_token.type)
+        ''
+      else
+        '}'
+      end
+    end
+
+    # Public: Search from this token to find the next token of a given type.
+    #
+    # type - A Symbol type of the token to find, or an Array of Symbols.
+    # opts - An optional Hash
+    #   :value       - A token value to search for in addition to type
+    #   :skip_blocks - A Boolean to specify whether { } blocks should be
+    #                  skipped over (defaults to true).
+    #
+    # Returns a PuppetLint::Lexer::Token object if a matching token could be
+    # found, otherwise nil.
+    def next_token_of(type, opts = {})
+      find_token_of(:next, type, opts)
+    end
+
+    # Public: Search from this token to find the previous token of a given type.
+    #
+    # type - A Symbol type of the token to find, or an Array of Symbols.
+    # opts - An optional Hash
+    #   :value       - A token value to search for in addition to type
+    #   :skip_blocks - A Boolean to specify whether { } blocks should be
+    #                  skipped over (defaults to true).
+    #
+    # Returns a PuppetLint::Lexer::Token object if a matching token could be
+    # found, otherwise nil.
+    def prev_token_of(type, opts = {})
+      find_token_of(:prev, type, opts)
+    end
+
+    # Internal: Search from this token to find the next token of a given type
+    # in a given direction.
+    #
+    # direction - A Symbol direction to search (:next or :prev).
+    # type      - A Symbol type of the token to find, or an Array of Symbols.
+    # opts      - An optional Hash
+    #   :value       - A token value to search for in addition to type
+    #   :skip_blocks - A Boolean to specify whether { } blocks should be
+    #                  skipped over (defaults to true).
+    #
+    # Returns a PuppetLint::Lexer::Token object if a matching token could be
+    # found, otherwise nil.
+    def find_token_of(direction, type, opts = {})
+      return nil unless [:next, :prev].include?(direction)
+
+      opts[:skip_blocks] ||= true
+      to_find = Array[*type]
+
+      token_iter = send("#{direction}_token".to_sym)
+      until token_iter.nil?
+        if to_find.include?(token_iter.type)
+          return token_iter if opts[:value].nil? || token_iter.value == opts[:value]
         end
-      end
 
-      def string_prefix
-        no_enclose_tokens = Set.new([:UNENC_VARIABLE, :DQPRE, :DQMID, :HEREDOC_PRE, :HEREDOC_MID])
-        if prev_token && no_enclose_tokens.include?(prev_token.type)
-          ''
-        else
-          '}'
-        end
-      end
+        opening_token = direction == :next ? 'L' : 'R'
+        closing_token = direction == :next ? 'R' : 'L'
 
-      # Public: Search from this token to find the next token of a given type.
-      #
-      # type - A Symbol type of the token to find, or an Array of Symbols.
-      # opts - An optional Hash
-      #   :value       - A token value to search for in addition to type
-      #   :skip_blocks - A Boolean to specify whether { } blocks should be
-      #                  skipped over (defaults to true).
-      #
-      # Returns a PuppetLint::Lexer::Token object if a matching token could be
-      # found, otherwise nil.
-      def next_token_of(type, opts = {})
-        find_token_of(:next, type, opts)
-      end
-
-      # Public: Search from this token to find the previous token of a given type.
-      #
-      # type - A Symbol type of the token to find, or an Array of Symbols.
-      # opts - An optional Hash
-      #   :value       - A token value to search for in addition to type
-      #   :skip_blocks - A Boolean to specify whether { } blocks should be
-      #                  skipped over (defaults to true).
-      #
-      # Returns a PuppetLint::Lexer::Token object if a matching token could be
-      # found, otherwise nil.
-      def prev_token_of(type, opts = {})
-        find_token_of(:prev, type, opts)
-      end
-
-      # Internal: Search from this token to find the next token of a given type
-      # in a given direction.
-      #
-      # direction - A Symbol direction to search (:next or :prev).
-      # type      - A Symbol type of the token to find, or an Array of Symbols.
-      # opts      - An optional Hash
-      #   :value       - A token value to search for in addition to type
-      #   :skip_blocks - A Boolean to specify whether { } blocks should be
-      #                  skipped over (defaults to true).
-      #
-      # Returns a PuppetLint::Lexer::Token object if a matching token could be
-      # found, otherwise nil.
-      def find_token_of(direction, type, opts = {})
-        return nil unless [:next, :prev].include?(direction)
-
-        opts[:skip_blocks] ||= true
-        to_find = Array[*type]
-
-        token_iter = send("#{direction}_token".to_sym)
-        until token_iter.nil?
-          if to_find.include?(token_iter.type)
-            return token_iter if opts[:value].nil? || token_iter.value == opts[:value]
+        if opts[:skip_blocks]
+          case token_iter.type
+          when "#{opening_token}BRACE".to_sym
+            token_iter = token_iter.send("#{direction}_token_of".to_sym, ["#{closing_token}BRACE".to_sym, opts])
+          when "#{opening_token}BRACK".to_sym
+            token_iter = token_iter.send("#{direction}_token_of".to_sym, ["#{closing_token}BRACK".to_sym, opts])
+          when "#{opening_token}PAREN".to_sym
+            token_iter = token_iter.send("#{direction}_token_of".to_sym, ["#{closing_token}PAREN".to_sym, opts])
           end
-
-          opening_token = direction == :next ? 'L' : 'R'
-          closing_token = direction == :next ? 'R' : 'L'
-
-          if opts[:skip_blocks]
-            case token_iter.type
-            when "#{opening_token}BRACE".to_sym
-              token_iter = token_iter.send("#{direction}_token_of".to_sym, ["#{closing_token}BRACE".to_sym, opts])
-            when "#{opening_token}BRACK".to_sym
-              token_iter = token_iter.send("#{direction}_token_of".to_sym, ["#{closing_token}BRACK".to_sym, opts])
-            when "#{opening_token}PAREN".to_sym
-              token_iter = token_iter.send("#{direction}_token_of".to_sym, ["#{closing_token}PAREN".to_sym, opts])
-            end
-          end
-
-          return nil if token_iter.nil?
-          token_iter = token_iter.send("#{direction}_token".to_sym)
         end
-        nil
-      end
 
-      def interpolated_variable?
-        return false if type == :TYPE && value != 'type'
-        return true if type == :NAME
-        PuppetLint::Lexer::KEYWORDS.include?(type.to_s.downcase)
+        return nil if token_iter.nil?
+        token_iter = token_iter.send("#{direction}_token".to_sym)
       end
+      nil
+    end
+
+    def interpolated_variable?
+      return false if type == :TYPE && value != 'type'
+      return true if type == :NAME
+      PuppetLint::Lexer::KEYWORDS.include?(type.to_s.downcase)
     end
   end
 end
