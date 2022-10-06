6adc98e19f17926613e1a34a449e6e4b100bcf56
Craig Gumbley <craiggumbley@gmail.com>
2022-10-06 12:10:27 +0100

    (MAINT) Rubocop fixes for spec

--- spec/puppet-lint_spec.rb
+++ spec/puppet-lint_spec.rb
@@ -1,16 +1,18 @@
+# rubocop: disable Naming/FileName
+
 require 'spec_helper'
 
 describe PuppetLint do
-  subject { PuppetLint.new }
+  subject(:linter) { described_class.new }
 
-  it 'should accept manifests as a string' do
-    subject.code = 'class foo { }'
-    expect(subject.code).to_not be_nil
+  it 'accepts manifests as a string' do
+    linter.code = 'class foo { }'
+    expect(linter.code).not_to be_nil
   end
 
-  it 'should return empty manifest when empty one given as the input' do
-    subject.code = ''
-    subject.run
-    expect(subject.manifest).to eq('')
+  it 'returns empty manifest when empty one given as the input' do
+    linter.code = ''
+    linter.run
+    expect(linter.manifest).to eq('')
   end
 end
