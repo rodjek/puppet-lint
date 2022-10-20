rodjek/puppet-lint

commit ed5ec22a5c6c33bdfd15f147b7c3db077096118a

(MAINT) Rubocop fixes and refactor load_from_gems
Previously load_from_gems contained nested loops and block
continuation. It was hard to read and probably not that efficient when
processing many files.

This commit cleans the method up by splitting the operations out in to
three distinct operations, hopefully simplifying the code slightly.

In addition, the Rubocop fixes have also been applied in this commit.

lib/puppet-lint/plugins.rb
@@ -1,77 +1,79 @@
 require 'pathname'
 
-class PuppetLint
-  # Public: Various methods that implement puppet-lint's plugin system
+# Public: Various methods that implement puppet-lint's plugin system
+#
+# Examples
+#
+#   PuppetLint::Plugins.load_spec_helper
+class PuppetLint::Plugins
+  # Internal: Find any gems containing puppet-lint plugins and load them.
   #
-  # Examples
-  #
-  #   PuppetLint::Plugins.load_spec_helper
-  class Plugins
-    # Internal: Find any gems containing puppet-lint plugins and load them.
-    #
-    # Returns nothing.
-    def self.load_from_gems
-      gem_directories.select { |path|
-        (path + 'puppet-lint/plugins').directory?
-      }.each do |gem_path|
-        Dir["#{gem_path + 'puppet-lint/plugins'}/*.rb"].each do |file|
-          load(file)
-        end
-      end
+  # Returns nothing.
+  def self.load_from_gems
+    plugins_directories = gem_directories.select do |directory|
+      (directory + 'puppet-lint/plugins').to_s if (directory + 'puppet-lint/plugins').directory?
     end
 
-    # Public: Load the puppet-lint spec_helper.rb
-    #
-    # Returns nothings.
-    def self.load_spec_helper
-      gemspec = gemspecs.select { |spec| spec.name == 'puppet-lint' }.first
-      load(Pathname.new(gemspec.full_gem_path) + 'spec/spec_helper.rb')
+    plugin_files = plugins_directories.each do |directory|
+      Dir["#{directory}/**/*.rb"]
     end
 
-    class << self
-      private
+    plugin_files.each do |file|
+      load(file)
+    end
+  end
 
-      # Internal: Check if RubyGems is loaded and available.
-      #
-      # Returns true if RubyGems is available, false if not.
-      def rubygems?
-        defined?(::Gem)
+  # Public: Load the puppet-lint spec_helper.rb
+  #
+  # Returns nothings.
+  def self.load_spec_helper
+    gemspec = gemspecs.find { |spec| spec.name == 'puppet-lint' }
+    load(Pathname.new(gemspec.full_gem_path) + 'spec/spec_helper.rb')
+  end
+
+  class << self
+    private
+
+    # Internal: Check if RubyGems is loaded and available.
+    #
+    # Returns true if RubyGems is available, false if not.
+    def rubygems?
+      defined?(::Gem)
+    end
+
+    # Internal: Retrieve a list of avaliable gemspecs.
+    #
+    # Returns an Array of Gem::Specification objects.
+    def gemspecs
+      @gemspecs ||= if Gem::Specification.respond_to?(:latest_specs)
+                      Gem::Specification.latest_specs(load_prerelease_plugins?)
+                    else
+                      Gem.searcher.init_gemspecs
+                    end
+    end
+
+    # Internal: Determine whether to load plugins that contain a letter in their version number.
+    #
+    # Returns true if the configuration is set to load "prerelease" gems, false otherwise.
+    def load_prerelease_plugins?
+      # Load prerelease plugins (which ruby defines as any gem which has a letter in its version number).
+      # Can't use puppet-lint configuration object here because this code executes before the command line is parsed.
+      if ENV['PUPPET_LINT_LOAD_PRERELEASE_PLUGINS']
+        return ['true', 'yes'].include?(ENV['PUPPET_LINT_LOAD_PRERELEASE_PLUGINS'].downcase)
       end
+      false
+    end
 
-      # Internal: Retrieve a list of avaliable gemspecs.
-      #
-      # Returns an Array of Gem::Specification objects.
-      def gemspecs
-        @gemspecs ||= if Gem::Specification.respond_to?(:latest_specs)
-                        Gem::Specification.latest_specs(load_prerelease_plugins?)
-                      else
-                        Gem.searcher.init_gemspecs
-                      end
-      end
-
-      # Internal: Determine whether to load plugins that contain a letter in their version number.
-      #
-      # Returns true if the configuration is set to load "prerelease" gems, false otherwise.
-      def load_prerelease_plugins?
-        # Load prerelease plugins (which ruby defines as any gem which has a letter in its version number).
-        # Can't use puppet-lint configuration object here because this code executes before the command line is parsed.
-        if ENV['PUPPET_LINT_LOAD_PRERELEASE_PLUGINS']
-          return ['true', 'yes'].include?(ENV['PUPPET_LINT_LOAD_PRERELEASE_PLUGINS'].downcase)
+    # Internal: Retrieve a list of available gem paths from RubyGems.
+    #
+    # Returns an Array of Pathname objects.
+    def gem_directories
+      if rubygems?
+        gemspecs.reject { |spec| spec.name == 'puppet-lint' }.map do |spec|
+          Pathname.new(spec.full_gem_path) + 'lib'
         end
-        false
-      end
-
-      # Internal: Retrieve a list of available gem paths from RubyGems.
-      #
-      # Returns an Array of Pathname objects.
-      def gem_directories
-        if rubygems?
-          gemspecs.reject { |spec| spec.name == 'puppet-lint' }.map do |spec|
-            Pathname.new(spec.full_gem_path) + 'lib'
-          end
-        else
-          []
-        end
+      else
+        []
       end
     end
   end

commit c455f9cb0994dd06e0fe4c4deaae105ebf96bda5

Fix plugin loading
In ed5ec22a5c6c33bdfd15f147b7c3db077096118a the method was refactored,
but it ended up only loading the plugin directory itself, which isn't a
file.

It now loads all Ruby files again

Fixes: ed5ec22a5c6c33bdfd15f147b7c3db077096118a

lib/puppet-lint/plugins.rb
@@ -10,16 +10,13 @@ class PuppetLint::Plugins
   #
   # Returns nothing.
   def self.load_from_gems
-    plugins_directories = gem_directories.select do |directory|
-      (directory + 'puppet-lint/plugins').to_s if (directory + 'puppet-lint/plugins').directory?
-    end
+    gem_directories.each do |directory|
+      path = directory + 'puppet-lint/plugins'
+      next unless path.directory?
 
-    plugin_files = plugins_directories.each do |directory|
-      Dir["#{directory}/**/*.rb"]
-    end
-
-    plugin_files.each do |file|
-      load(file)
+      Dir["#{path}/**/*.rb"].each do |file|
+        load(file)
+      end
     end
   end
 