rodjek/puppet-lint

commit 7f67d8cdb30ddc6efef9333c11e49a530197cee3

(MAINT) Bump min ruby version & cleanup
This commit bumps the minimum required ruby version to 2.5. It also
contains some small cleanups. Mainly removal of older version
constraints.

puppet-lint.gemspec
@@ -1,20 +1,35 @@
 $LOAD_PATH.push(File.expand_path('../lib', __FILE__))
 require 'puppet-lint/version'
 
-Gem::Specification.new do |s|
-  s.name = 'puppet-lint'
-  s.version = PuppetLint::VERSION.dup
-  s.homepage = 'https://github.com/puppetlabs/puppet-lint/'
-  s.summary = 'Ensure your Puppet manifests conform with the Puppetlabs style guide'
-  s.description = 'Checks your Puppet manifests against the Puppetlabs
-  style guide and alerts you to any discrepancies.'
+Gem::Specification.new do |spec|
+  spec.name = 'puppet-lint'
+  spec.version = PuppetLint::VERSION.dup
+  spec.homepage = 'https://github.com/puppetlabs/puppet-lint/'
+  spec.summary = 'Ensure your Puppet manifests conform with the Puppetlabs style guide'
+  spec.description = <<-EOF
+    Checks your Puppet manifests against the Puppetlabs style guide and alerts you to any discrepancies.'
+  EOF
 
-  s.files = Dir['CHANGELOG.md', 'HISTORY.md', 'README.md', 'LICENSE', 'lib/**/*', 'bin/**/*', 'spec/**/*']
-  s.test_files = s.files.grep(%r{\Aspec/})
-  s.executables = s.files.grep(%r{\Abin/}) { |f| File.basename(f) }
-  s.require_paths = ['lib']
+  spec.files = Dir[
+    'README.md',
+    'LICENSE',
+    'lib/**/*',
+    'bin/**/*',
+    'spec/**/*',
+  ]
+  spec.executables = Dir['bin/**/*'].map { |f| File.basename(f) }
+  spec.require_paths = ['lib']
 
-  s.authors = ['Tim Sharpe', 'Puppet, Inc.', 'Community Contributors']
-  s.email = ['tim@sharpe.id.au', 'modules-team@puppet.com']
-  s.license = 'MIT'
+  spec.authors = [
+    'Tim Sharpe',
+    'Puppet, Inc.',
+    'Community Contributors',
+  ]
+  spec.email = [
+    'tim@sharpe.id.au',
+    'modules-team@puppet.com',
+  ]
+  spec.license = 'MIT'
+
+  spec.required_ruby_version = Gem::Requirement.new(">= 2.7".freeze)
 end

Gemfile
@@ -7,39 +7,22 @@ group :test do
   gem 'rspec-its', '~> 1.0'
   gem 'rspec-collection_matchers', '~> 1.0'
 
-  if RUBY_VERSION < '2.0'
-    # json 2.x requires ruby 2.0. Lock to 1.8
-    gem 'json', '= 1.8'
-    # json_pure 2.0.2 requires ruby 2.0, and 2.0.1 requires ruby 1.9. Lock to 1.8.3.
-    gem 'json_pure', '= 1.8.3'
-    # addressable 2.4.0 requires ruby 1.9.0. Lock to 2.3.8.
-    gem 'addressable', '= 2.3.8'
-    gem 'diff-lcs', '< 1.3'
-    gem 'rspec', '<= 3.4'
-  else
-    gem 'rspec', '~> 3.0'
-    gem 'json'
-  end
+  gem 'rspec', '~> 3.0'
+  gem 'json'
 
-  if RUBY_VERSION > '1.8'
-    # requires ruby 1.9+, on 1.8 we'll fall back to the old regex parsing
-    gem 'rspec-json_expectations', '~> 1.4'
-  end
+  gem 'rspec-json_expectations', '~> 1.4'
 
-  gem 'rubocop', '0.49.1' if RUBY_VERSION > '2.0'
   gem 'simplecov', :require => false if ENV['COVERAGE'] == 'yes'
 end
 
 group :development do
-  if RUBY_VERSION > '1.9'
-    # For Changelog generation
-    if RUBY_VERSION >= '2.2.2'
-      gem 'github_changelog_generator', :require => false
-    else
-      gem 'github_changelog_generator', '~> 1.13.0', :require => false
-      gem 'rack', '~> 1.0', :require => false
-    end
+    gem 'github_changelog_generator', require: false
+    gem 'faraday-retry', require: false
+    gem 'pry', require: false
+end
 
-    gem 'pry'
-  end
+group :rubocop do
+    gem 'rubocop', '~> 1.6.1', require: false
+    gem 'rubocop-rspec', '~> 2.0.1', require: false
+    gem 'rubocop-performance', '~> 1.9.1', require: false
 end

Rakefile
@@ -1,15 +1,14 @@
-require 'rake'
-require 'rspec/core/rake_task'
-require 'puppet-lint'
-require 'puppet-lint/tasks/release_test'
 require 'bundler/gem_tasks'
-
-task :default => :test
-
-RSpec::Core::RakeTask.new(:test)
+require 'rubocop/rake_task'
+require 'github_changelog_generator/task'
+require 'puppet-lint/version'
+require 'rspec/core/rake_task'
 
 begin
   require 'github_changelog_generator/task'
+rescue LoadError
+  # Gem not present
+else
   GitHubChangelogGenerator::RakeTask.new(:changelog) do |config|
     version = PuppetLint::VERSION
     config.user = 'puppetlabs'
@@ -19,28 +18,8 @@ begin
     config.exclude_labels = %w[duplicate question invalid wontfix release-pr documentation]
     config.enhancement_labels = %w[feature]
   end
-rescue LoadError
-  $stderr.puts 'Changelog generation requires Ruby 2.0 or higher'
 end
 
-begin
-  require 'rubocop/rake_task'
+RSpec::Core::RakeTask.new(:spec)
+task :default => :test
 
-  RuboCop::RakeTask.new(:rubocop) do |task|
-    task.options = %w[-D -E]
-    task.patterns = [
-      'lib/**/*.rb',
-      'spec/**/*.rb',
-      'bin/*',
-      '*.gemspec',
-      'Gemfile',
-      'Rakefile',
-    ]
-  end
-rescue LoadError
-  $stderr.puts 'Rubocop is not available for this version of Ruby.'
-end
-
-task :ci => [:test, :release_test]
-
-# vim: syntax=ruby

commit f40899d8c7b6a884e49574fb4c5aa29e197406ed

Remove spurious quote in gem description
Fixes: 7f67d8cdb30ddc6efef9333c11e49a530197cee3

puppet-lint.gemspec
@@ -7,7 +7,7 @@ Gem::Specification.new do |spec|
   spec.homepage = 'https://github.com/puppetlabs/puppet-lint/'
   spec.summary = 'Ensure your Puppet manifests conform with the Puppetlabs style guide'
   spec.description = <<-EOF
-    Checks your Puppet manifests against the Puppetlabs style guide and alerts you to any discrepancies.'
+    Checks your Puppet manifests against the Puppetlabs style guide and alerts you to any discrepancies.
   EOF
 
   spec.files = Dir[